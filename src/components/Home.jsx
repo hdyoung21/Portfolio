990653ef63279908540bda99862016aeed95dfe9
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2023-03-29 14:05:04 -0400

    dropdown

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-[#ffd700]'>Welcome to the protfolio of</p>
                 <h1 className='text-white font-bold text-4xl sm:text-7xl'>Hunter Young</h1>
-                <h2 className='text-[#807f77] font-bold text-3xl sm:text-7xl'>I'm a Full Stack Developer.</h2>
+                <h2 className='text-[#807f77] font-bold text-3xl sm:text-7xl'>Full Stack Developer.</h2>
                 <p className='text-[#ffd700] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus sapiente rem et explicabo at illum eligendi, nemo quidem animi ipsum dolores iste fuga adipisci corrupti ea accusamus aspernatur doloribus!</p>
                 <div>
                     <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black'>View Work <HiArrowNarrowRight className='ml-2'/></button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,13 +18,29 @@ const Navbar = () => {
             <ul className='hidden md:flex text-[#ffd700]'>
                 <li>
                     <Link to="home" smooth={true} duration={500}>
-                    Home
+                        Home
                     </Link>
                 </li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true} duration={500}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact Me
+                    </Link>
+                </li>
             </ul>
 
             {/* burger */}
@@ -33,11 +49,28 @@ const Navbar = () => {
             </div>
             {/* mobile menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#154b2bfb] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Projects</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link to="work" smooth={true} duration={500}>
+                        Projects
+                    </Link></li>
+                <li className='py-6 text-4xl'>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact Me
+                    </Link>
+                </li>
             </ul>
 
             {/* social icons */}

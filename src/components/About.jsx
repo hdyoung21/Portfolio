920f6a0732b68ae2a55f245b1d0f51f5d970c696
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2023-03-29 07:52:02 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,26 +4,26 @@ function About() {
     return (
         <div name='about' className='w-full h-screen bg-[#154b2bfb] text-gray-300'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
-                <div className='max-w-[1000px] w-full px-4 grid grid-col-2 gap-8'>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                     <div className='sm:text-right pb-8 pl-4'>
-                        <p className='text-4xl font-bold inline border-b-slate-400'>
+                        <p className='text-4xl font-bold inline border-b-4'>
                             About
-                            </p>
+                        </p>
                     </div>
                     <div></div>
+                </div>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                    <div className='sm:text-right text-4xl font-bold text-[#807f77]'>
+                        <p>Hey, Im Hunter Young and thanks for checking out my porfolio</p>
                     </div>
-                    <div className='max-w-[1000px] w-full px-4 grid grid-col-2 gap-8 '>
-                        <div>
-                            <p>Hi info info info</p>
-                        </div>
-                        <div>
-                            <p>I was born and raised in Atlanta, Ga.  I have always been passionate about building things and the outdoors.  Over the past few years I have developed an affinity for technology that I hadnt had before.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut provident ab temporibus labore architecto accusantium quaerat odio aspernatur tempora? Esse beatae recusandae debitis temporibus exercitationem aspernatur vitae eveniet illo quibusdam.</p>
-                        </div>
+                    <div>
+                        <p className='text-[#ffd700]'>I was born and raised in Atlanta, Ga.  I have always been passionate about building things and the outdoors.  Over the past few years I have developed an affinity for technology that I hadnt had before.  It began when I started with an access-control company and was installing and selling camera systems and badge readers.  As I became more knowledgeable and worked with more inovative products my curisoity grew.  I first got into networking and earned my certification, but i wasnt in love with it.  In 2022 I enrolled Georgia Tech's Full Stack Developer program and have been enjoying building and collaboring from day 1, excited to be learing and growing in a new field to myself.  I look forward to adding more about my coding journey and sharing the projects I have built along the way.</p>
+                    </div>
 
-                    </div>
                 </div>
             </div>
-        
+        </div>
+
     )
 }
 

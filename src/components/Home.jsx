7788fc63d474b699d283de81c4ce3be4b28ebfb5
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2023-03-29 14:52:56 -0400

    wooo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,20 @@ function Home() {
 
             {/* container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-[#ffd700]'>Welcome to the protfolio of</p>
+                <p className='text-[#ffd700]'>Welcome to the portfolio of</p>
                 <h1 className='text-white font-bold text-4xl sm:text-7xl'>Hunter Young</h1>
                 <h2 className='text-[#807f77] font-bold text-3xl sm:text-7xl'>Full Stack Developer.</h2>
-                <p className='text-[#ffd700] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus sapiente rem et explicabo at illum eligendi, nemo quidem animi ipsum dolores iste fuga adipisci corrupti ea accusamus aspernatur doloribus!</p>
+                <p className='text-[#ffd700] py-4 max-w-[700px]'>Emerging developer with a passion for developing client-focused applications with polished front-ends and robust backends.  Enjoy the out-of-the-box thinking needed for creative problem solving.  Energized by collaborating with customers and colleagues to solve business challenges. Looking for a company that I can invest in as they invest in me to grow and develop my technical and consulting skills.</p>
                 <div>
-                    <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black'>View Work <HiArrowNarrowRight className='ml-2'/></button>
+                    <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black hover:border-[#ffd700]'>
+                        View Work
+                        <span className='hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3' />
+                        </span>
+                    </button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,24 +50,24 @@ const Navbar = () => {
             {/* mobile menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#154b2bfb] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>
-                    <Link to="home" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
                         Home
                     </Link>
                 </li>
                 <li className='py-6 text-4xl'>
-                    <Link to="about" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
                         About
                     </Link></li>
                 <li className='py-6 text-4xl'>
-                    <Link to="skills" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
                         Skills
                     </Link></li>
                 <li className='py-6 text-4xl'>
-                    <Link to="work" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
                         Projects
                     </Link></li>
                 <li className='py-6 text-4xl'>
-                    <Link to="contact" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
                         Contact Me
                     </Link>
                 </li>
@@ -77,22 +77,22 @@ const Navbar = () => {
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-blue-500'>
-                        <a className='flex justify-between items-center w-full text-white' href='/'>
+                        <a className='flex justify-between items-center w-full text-white' href='https://www.linkedin.com/in/hunter-young-688130125/'>
                             Linkedin <FaLinkedin size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-black'>
-                        <a className='flex justify-between items-center w-full text-white' href='/'>
+                        <a className='flex justify-between items-center w-full text-white' href='https://github.com/hdyoung21'>
                             Github <FaGithub size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-red-500'>
-                        <a className='flex justify-between items-center w-full text-white' href='/'>
+                    {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-red-500'>
+                        <a className='flex justify-between items-center w-full text-white' href='/contact'>
                             Email <HiOutlineMail size={30} />
                         </a>
-                    </li>
+                    </li> */}
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-white'>
-                        <a className='flex justify-between items-center w-full text-black' href='/'>
+                        <a className='flex justify-between items-center w-full text-black' href='https://docs.google.com/document/d/1OR-cb4Df5HP6ER_c9Uw6vQ4cWZxHEiuq/edit?usp=sharing&ouid=103502477606550281684&rtpof=true&sd=true'>
                             Resume <BsPersonBadgeFill size={30} />
                         </a>
                     </li>

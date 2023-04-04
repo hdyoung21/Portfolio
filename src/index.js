97861ede6ebf38d3a33625f37c0830c15d827af7
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2023-04-04 17:13:15 -0400

    ijs

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+  ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,

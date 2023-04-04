06560b15790904465c62369f2bcbc174f22655d6
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2023-04-04 16:25:11 -0400

    homepage

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'app.js'));
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
     console.log('server is up on port ${port}!')
